c9db101107b9ef725549cb8907d58f176933d893
elenavagyan <166144811+elenavagyan@users.noreply.github.com>
2025-01-13 22:01:30 +0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,41 +1,38 @@
 from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
 class Product(Base):
-    __tablename__ = 'product'
-    
-    product_id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100), index=True)
-    manufacturer = Column(String(100))
-    unit = Column(String(50))  # единицы измерения
-    
-    def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    __tablename__ = 'products'
+
+    product_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), index=True, nullable=False)
+    manufacturer = Column(String(100), nullable=False)
+    unit = Column(String(50), nullable=False)
+
+    purchases = relationship("Purchase", back_populates="product")
 
 class Customer(Base):
-    __tablename__ = 'customer'
-    
-    customer_id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100), index=True)
-    address = Column(String(200))
-    phone = Column(String(50))
-    contact_person = Column(String(100))
-    
-    def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    __tablename__ = 'customers'
+
+    customer_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), index=True, nullable=False)
+    address = Column(String(200), nullable=False)
+    phone = Column(String(50), nullable=False)
+    contact_person = Column(String(100), nullable=False)
+
+    purchases = relationship("Purchase", back_populates="customer")
 
 class Purchase(Base):
-    __tablename__ = 'purchase'
-    
-    purchase_id = Column(Integer, primary_key=True, autoincrement=True)
-    product_id = Column(Integer, ForeignKey('product.product_id'))
-    customer_id = Column(Integer, ForeignKey('customer.customer_id'))
-    quantity = Column(Float)
-    delivery_date = Column(DateTime)
-    price_per_unit = Column(Float)
-    
-    def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    __tablename__ = 'purchases'
+
+    purchase_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    product_id = Column(Integer, ForeignKey('products.product_id'), nullable=False)
+    customer_id = Column(Integer, ForeignKey('customers.customer_id'), nullable=False)
+    quantity = Column(Float, nullable=False)
+    delivery_date = Column(DateTime, nullable=False)
+    price_per_unit = Column(Float, nullable=False)
+
+    product = relationship("Product", back_populates="purchases")
+    customer = relationship("Customer", back_populates="purchases")
